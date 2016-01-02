3ecf1465fd4a8215a8151493638b3851c1158fbe
shrutisethia04 <shrutisethia04@gmail.com>
2016-01-02 19:24:38 +0530

    Shruti changes
    Same hai bus ab login ka link hai(hardcoded login) username is admin and
    password is pass

--- forum_practice/hw.py
+++ forum_practice/hw.py
@@ -1,13 +1,26 @@
-from flask import Flask,render_template
-
-
+from flask import *
 app=Flask(__name__)
 @app.route('/')
 def index():
     return render_template('home.html')
+
 @app.route('/welcome')
 def profile():
-    return render_template('welcome.html')	
+    return render_template('welcome.html')
+	
+@app.route('/hello')
+def hello():
+    return render_template('hello.html')
+	
+@app.route('/log',methods=['GET','POST'])
+def log():
+        error = None
+        if request.method == 'POST':
+            if request.form['user'] != 'admin' or request.form['pass'] != 'pass':
+                error = "Invalid Credentials entered !!"
+            else:
+                return redirect(url_for('hello'))	
+        return render_template('log.html',error=error)		
 if __name__ == "__main__":
     app.run(debug=True)	
 	
